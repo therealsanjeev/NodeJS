0e9f8b2fff51a0a668bfbe671b8d06dba0e29213
sanjeev <therealsanjeev0@gmail.com>
2022-01-07 01:58:10 +0530

    waether-app init

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,9 +1,9 @@
 const request=require('request')
 
-const baseUrl='http://api.weatherstack.com/current?access_key=df1294a4a0e5612dc74c7907e14222d8&query=New%20York'
+const baseUrl='http://api.weatherstack.com/current?access_key=df1294a4a0e5612dc74c7907e14222d8&query=New%20York&units=f'
 
-request({url:baseUrl},(error,response)=>{
-    const data=JSON.parse(response.body)
-    console.log(data.location)
+request({url:baseUrl,json:true},(error,response)=>{
+    // const data=JSON.parse(response.body)
+    console.log(response.body.current.weather_descriptions[0])
 
 })
