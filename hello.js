da552f4a050dbbd5a6a3f5bafa8424e67aa22a03
sanjeev <therealsanjeev0@gmail.com>
2022-01-08 16:21:24 +0530

    first callback fun

--- hello.js
+++ hello.js
@@ -1,1 +1,13 @@
 console.log("Hello Node.JS!")
+
+// call back function
+
+const add=(a,b,callback)=>{
+    setTimeout(()=>{
+        callback(a+b)
+    },2000)
+}
+
+add(4,6,(sum)=>{
+    console.log(sum);
+})
