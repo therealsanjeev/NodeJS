68529db326a5f6ba1bd798fabe9c4483ce4ae559
sanjeev <therealsanjeev0@gmail.com>
2022-01-08 16:06:27 +0530

    error handling

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,6 +4,28 @@ const baseUrl='http://api.weatherstack.com/current?access_key=df1294a4a0e5612dc7
 
 request({url:baseUrl,json:true},(error,response)=>{
     // const data=JSON.parse(response.body)
+    if(error){
+        console.log('Unable to connect, try again!')
+    }else if(response.body.error){
+        console.log(response.body.error.info)
+    }else
     console.log(response.body.current.weather_descriptions[0])
+})
 
+const geoCoding='https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoidGhlcmVhbHNhbmplZXYiLCJhIjoiY2t5M2V5MHpiMDE2NTJ2cHYxNHE0YmRpMCJ9.BHox9GnMrliagz1VQ_y_Tw&limit=1'
+
+const t='https://api.mapbox.com/geocoding/v5/mapbox.places/ges232es.json?access_token=pk.eyJ1IjoidGhlcmVhbHNhbmplZXYiLCJhIjoiY2t5M2V5MHpiMDE2NTJ2cHYxNHE0YmRpMCJ9.BHox9GnMrliagz1VQ_y_Tw&limit=1'
+request({url:geoCoding,json:true},(error,response)=>{
+    // const data=JSON.parse(response.body)
+
+    if(error){
+        console.log('Unable to connect, try again!')
+    }else if(response.body.features.length==0){
+        console.log('Invalid request:(')
+    }else{
+        const latitude=response.body.features[0].center[0]
+        const longitude=response.body.features[0].center[1]
+        console.log(latitude,longitude)
+    }
+   
 })
